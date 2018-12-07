755137d8ef9d4e3bf44dfe5795ff5bd85c2477b5
Ryan McLaughlin <rmclaughlin@shamwow.microbiology.ubc.ca>
2018-12-07 15:44:51 -0800

    added UMAP

--- sagmag.py
+++ sagmag.py
@@ -71,7 +71,7 @@ def tetra_cnt(seq_list):
 		tmp_dict = {k: 0 for k, v in tetra_cnt_dict.items()}
 		total_kmer_cnt = 0
 		clean_seq = seq.strip('\n').lower()
-		kmer_list = [''.join(x) for x in get_tetra(clean_seq)]
+		kmer_list = [''.join(x) for x in get_kmer(clean_seq, 4)]
 		for tetra in tmp_dict.keys():
 			count_tetra = kmer_list.count(tetra)
 			tmp_dict[tetra] = count_tetra
@@ -87,14 +87,14 @@ def tetra_cnt(seq_list):
 		tetra_prop_dict = {}
 		for tetra in dedup_dict.keys():
 			if dedup_dict[tetra] != '':
-				tetra_prop_dict[tetra] = tmp_dict[tetra] + tmp_dict[dedup_dict[tetra]]
-				#t_prop = (tmp_dict[tetra] 
-				#			+ tmp_dict[dedup_dict[tetra]]) / total_kmer_cnt
-				#tetra_prop_dict[tetra] = t_prop
+				#tetra_prop_dict[tetra] = tmp_dict[tetra] + tmp_dict[dedup_dict[tetra]]
+				t_prop = (tmp_dict[tetra] 
+							+ tmp_dict[dedup_dict[tetra]]) / total_kmer_cnt
+				tetra_prop_dict[tetra] = t_prop
 			else:
-				tetra_prop_dict[tetra] = tmp_dict[tetra]
-				#t_prop = tmp_dict[tetra] / total_kmer_cnt
-				#tetra_prop_dict[tetra] = t_prop
+				#tetra_prop_dict[tetra] = tmp_dict[tetra]
+				t_prop = tmp_dict[tetra] / total_kmer_cnt
+				tetra_prop_dict[tetra] = t_prop
 		# add to tetra_cnt_dict
 		for k in tetra_cnt_dict.keys():
 			if k in tetra_prop_dict.keys():
@@ -106,21 +106,74 @@ def tetra_cnt(seq_list):
 	dedupped_df = tetra_cnt_df.loc[:, (tetra_cnt_df != 0.0).any(axis=0)]
 	return dedupped_df
 
-def get_tetra(seq, n=4):  # found on internet
-    "Returns a sliding window (of width n) over data from the iterable"
-    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
-    it = iter(seq)
-    result = tuple(islice(it, n))
-    if len(result) == n:
-        yield result    
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
+
+def get_kmer(seq, n):  # found on internet
+	"Returns a sliding window (of width n) over data from the iterable"
+	"   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...				   "
+	it = iter(seq)
+	result = tuple(islice(it, n))
+	if len(result) == n:
+		yield result
+	for elem in it:
+		result = result[1:] + (elem,)
+		yield result
+
+
+def get_frags(seq, l_max):  # not sure about this function, probs shouldn't use it
+	"Fragments seq into subseqs of length l_max,"
+	"Leftover tail overlaps with tail-1"
+	seq_frags = [seq[i:i+l_max] for i in
+					 range(0, len(seq), l_max)]
+	tail_frag = seq[-l_max:]
+	seq_frags[-1] = tail_frag
+	return seq_frags
+
+def get_subseqs(seqs, n):
+	all_sub_seqs = []
+	for seq in seqs:
+		clean_seq = seq.strip('\n').lower()
+		sub_list = get_frags(clean_seq, n)
+		all_sub_seqs.extend(sub_list)	
+	return all_sub_seqs
+
+
+def plot_umap(df, n_neighbors=15, min_dist=0.1,
+				n_components=2, metric='euclidean', title=''):
+	fit = umap.UMAP(
+		n_neighbors=n_neighbors,
+		min_dist=min_dist,
+		n_components=n_components,
+		metric=metric
+	)
+	features = concat_df.values
+	targets = concat_df.index.values
+	colors = ['r', 'g', 'b']
+	color_list = [colors[x[0]] for x in enumerate(set(targets))]
+
+	u = fit.fit_transform(features);
+	fig = plt.figure()
+	if n_components == 1:
+		ax = fig.add_subplot(111)
+		ax.scatter(u[:,0], range(len(u)), color=color_list)
+	if n_components == 2:
+		ax = fig.add_subplot(111)
+		ax.scatter(u[:,0], u[:,1], color=color_list)
+	if n_components == 3:
+		ax = fig.add_subplot(111, projection='3d')
+		ax.scatter(u[:,0], u[:,1], u[:,2], color=color_list, s=100)
+	plt.title(title, fontsize=18)
+	plot_file_name = '_'.join([str(n_neighbors), str(min_dist),
+								str(n_components), metric]) + '.png'
+	plt.savefig(plot_file_name)
+	plt.clf()
+
 
 
 ### Start Main ###
 sag_fasta = sys.argv[1]
 mg_fasta = sys.argv[2]
+max_contig_len = int(sys.argv[3])
+print('Max contig size is %s bp' % max_contig_len)
 
 # Process the SAG fasta
 sag_contigs = []
@@ -132,12 +185,13 @@ with open(sag_fasta, 'r') as f:
 		seq = ''.join(split_rec[1:])
 		if seq != '':
 			sag_contigs.append(seq)
-sag_tetra_df = pd.DataFrame.from_dict(tetra_cnt(sag_contigs))
+# Break up contigs into overlapping subseqs
+sag_subs = get_subseqs(sag_contigs, max_contig_len)
+
+sag_tetra_df = pd.DataFrame.from_dict(tetra_cnt(sag_subs))
 sag_tetra_df['contig_id'] = ['sag_0' for x in sag_tetra_df.index]
 sag_tetra_df.set_index('contig_id', inplace=True)
-print(sag_tetra_df.head())
 print('SAG tetranucleotide frequencies calculated')
-
 # Process the MetaG fasta
 mg_contigs = []
 with open(mg_fasta, 'r') as f:
@@ -148,18 +202,20 @@ with open(mg_fasta, 'r') as f:
 		seq = ''.join(split_rec[1:])
 		if seq != '':
 			mg_contigs.append(seq)
-mg_tetra_df = pd.DataFrame.from_dict(tetra_cnt(mg_contigs))
+# Break up contigs into overlapping subseqs
+mg_subs = get_subseqs(mg_contigs, max_contig_len)
+
+mg_tetra_df = pd.DataFrame.from_dict(tetra_cnt(mg_subs))
 mg_tetra_df['contig_id'] = ['contig_0' for x in mg_tetra_df.index]
 mg_tetra_df.set_index('contig_id', inplace=True)
-print(mg_tetra_df.head())
 print('Metagenome tetranucleotide frequencies calculated')
 
 concat_df = pd.concat([sag_tetra_df, mg_tetra_df])
-print(concat_df.shape)
 
 # Don't need to scale tetra Hz data, I think?
 
 # PCA
+'''
 features = concat_df.values
 targets = concat_df.index.values
 x = StandardScaler().fit_transform(features)
@@ -167,10 +223,52 @@ x = StandardScaler().fit_transform(features)
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 pc_df = pd.DataFrame(data = principalComponents
-             , columns = ['principal component 1', 'principal component 2'])
+			 , columns = ['principal component 1', 'principal component 2'])
 final_df = pc_df.set_index(concat_df.index)
 print(final_df.head())
 
 ax = sns.scatterplot(x="principal component 1", y="principal component 2",
 						hue=final_df.index, data=final_df)
-plt.savefig('output.png')
+plt.savefig('standard_pca.png')
+plt.clf()
+'''
+
+### Try UMAP ###
+import numpy as np
+from sklearn.datasets import load_iris, load_digits
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+
+import umap
+
+#reducer = umap.UMAP(
+#					n_neighbors=15,
+#					min_dist=0.0,
+#					n_components=2
+#					)
+
+for n in (2, 5, 10, 20, 50, 100, 200):
+	plot_umap(concat_df, n_neighbors=n, title='n_neighbors = {}'.format(n))
+for d in (0.0, 0.1, 0.25, 0.5, 0.8, 0.99):
+    plot_umap(concat_df, min_dist=d, title='min_dist = {}'.format(d))
+
+plot_umap(concat_df, n_components=1, title='n_components = 1')
+plot_umap(concat_df, n_components=2, title='n_components = 2')
+plot_umap(concat_df, n_components=3, title='n_components = 3')
+
+#features = concat_df.values
+#targets = concat_df.index.values
+#color_list = [x[0] for x in enumerate(set(targets))]
+
+#embedding = reducer.fit_transform(features)
+
+#ax = sns.scatterplot(x=embedding[:, 0], y=embedding[:, 1], hue=targets)
+#plt.gca().set_aspect('equal', 'datalim')
+#plt.title('UMAP projection of SAG and MetaG contigs', fontsize=24)
+#plt.savefig('UMAP_plot.png')
+#plt.clf()
+
