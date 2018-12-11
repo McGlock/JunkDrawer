ed87fc2c5676b24596ed5b30c403ca9b5c400526
Ryan McLaughlin <rmclaughlin@shamwow.microbiology.ubc.ca>
2018-12-10 19:48:12 -0800

    fiddling

--- sagmag.py
+++ sagmag.py
@@ -1,15 +1,31 @@
 import sys
+from os import listdir
+from os.path import isfile, join
 from itertools import islice
-import csv
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 import pandas as pd
+import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import seaborn as sns
 import matplotlib.pyplot as plt
-from os import listdir
-from os.path import isfile, join
+import hdbscan
+import umap
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.datasets import load_iris, load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+import sklearn.cluster as cluster
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import LeaveOneOut
+from sklearn.neighbors import KernelDensity
+from sklearn.base import BaseEstimator, ClassifierMixin
+import pickle
+
+
+
+sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 
 
 
@@ -132,45 +148,50 @@ def get_frags(seq, l_max, o_lap):  # not sure about this function, probs shouldn
 				frag = seq[i:i+l_max]
 			else:
 				frag = seq[-l_max:]
-			seq_frags.append(frag)
-	else:
+			if 'n' not in frag:
+				seq_frags.append(frag)
+	elif 'n' not in seq:
 		seq_frags.append(seq)
-	#seq_frags = [seq[i:i+l_max] for i in
-	#				 range(0, len(seq), l_max)]
-	#tail_frag = seq[-l_max:]
-	#seq_frags[-1] = tail_frag
 	return seq_frags
 
-def get_subseqs(seqs, n, o_lap):
+def get_subseqs(seq_list, n, o_lap):
 	all_sub_seqs = []
-	for seq in seqs:
+	all_sub_headers = []
+	for seq_tup in seq_list:
+		header, seq = seq_tup
 		clean_seq = seq.strip('\n').lower()
 		sub_list = get_frags(clean_seq, n, o_lap)
-		all_sub_seqs.extend(sub_list)	
-	return all_sub_seqs
+		sub_headers = [header + '_' + str(i) for i, x in enumerate(sub_list, start=0)]
+		all_sub_seqs.extend(sub_list)
+		all_sub_headers.extend(sub_headers)	
+	return all_sub_headers, all_sub_seqs
 
 
-def plot_umap(df, n_neighbors=15, min_dist=0.1,
-				n_components=2, metric='euclidean', title=''):
+def plot_umap(df, sv_pth='./', n_neighbors=15, min_dist=0.1,
+				n_components=2, metric='euclidean', random_state=42,
+				title=''):
 	fit = umap.UMAP(
 		n_neighbors=n_neighbors,
 		min_dist=min_dist,
 		n_components=n_components,
-		metric=metric
-	)
-	features = concat_df.values
-	targets = concat_df.index.values
+		metric=metric,
+		random_state=random_state
+		)
+	features = df.values
+	targets = df.index.values
 	targets_ints = [x[0] for x in enumerate(targets, start=0)]
 
-	embedding = fit.fit_transform(features, y=targets_ints)
+	embedding = fit.fit_transform(features)
 	ax = sns.scatterplot(x=embedding[:, 0], y=embedding[:, 1], hue=targets)
 	plt.gca().set_aspect('equal', 'datalim')
 	plt.title(title, fontsize=18)
 	plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 	plot_file_name = '_'.join([str(n_neighbors), str(min_dist),
 								str(n_components), metric]) + '.png'
-	plt.savefig(plot_file_name, bbox_inches="tight")
+	plot_save_path = join(sv_pth, plot_file_name)
+	plt.savefig(plot_save_path, bbox_inches="tight")
 	plt.clf()
+	return embedding
 
 
 def get_seqs(fasta_file):
@@ -178,30 +199,206 @@ def get_seqs(fasta_file):
 	with open(fasta_file, 'r') as f:
 		data = f.read()
 		split_data = data.split('>')
-		for reccord in split_data:
-			split_rec = reccord.split('\n')
+		for record in split_data:
+			split_rec = record.split('\n')
+			header = split_rec[0]
 			seq = ''.join(split_rec[1:])
 			if seq != '':
-				sag_contigs.append(seq)
+				sag_contigs.append((header, seq))
 	return sag_contigs
 
 
+def calc_seg(subseqs):
+	seg_list = []
+	for seq in subseqs:
+		seg_sum = 0
+		for i, nuc in enumerate(seq, start=0):
+			seg_sum += calc_nuc(nuc, i)
+		seg_list.append(seg_sum)
+	return seg_list
+
+
+def calc_nuc(nuc, ind):
+	nuc_dict = {'a': 0, 't': 1, 'c': 2, 'g': 3}
+	nuc_hash = nuc_dict[nuc] * (4**ind)
+	return nuc_hash
+
+
 ### Start Main ###
-sag_path = sys.argv[1]
-#mg_fasta = sys.argv[3]
-max_contig_len = int(sys.argv[2])
-overlap_len = int(sys.argv[3])
+#sag_path = sys.argv[1]
+sag_file = sys.argv[1]
+mg_file = sys.argv[2]
+max_contig_len = int(sys.argv[3])
+overlap_len = int(sys.argv[4])
+save_path = sys.argv[5]
+
 if max_contig_len > 0:
 	print('Max contig size is %s bp' % max_contig_len)
 else:
 	print('Not fragmenting contigs')
 
+# SAG Tetras
+sag_id = sag_file.split('/')[-1].split('.')[0]
+'''
+sag_contigs = get_seqs(sag_file)
+# Break up contigs into overlapping subseqs
+sag_headers, sag_subs = get_subseqs(sag_contigs, max_contig_len, overlap_len)
+sag_tetra_df = pd.DataFrame.from_dict(tetra_cnt(sag_subs))
+sag_tetra_df['contig_id'] = sag_headers
+sag_tetra_df.set_index('contig_id', inplace=True)
+print('SAG %s tetranucleotide frequencies calculated' % sag_id)
+sag_tetra_df.to_csv(join(save_path, sag_id + '.tsv'), sep='\t')
+'''
+sag_tetra_df = pd.read_csv(join(save_path, sag_id + '.tsv'), sep='\t', index_col=0,
+						header=0)
+print('Opened SAG tetranucleotide tsv file')
+'''
+# SAG L-seg hash
+tmp, sag_Ls = get_subseqs(sag_contigs, 24, 23)
+sag_hashes = calc_seg(sag_Ls)
+sag_hashes.sort(reverse=True)
+sag_hashes_set = set(sag_hashes)
+with open(join(save_path, sag_id + '.pkl'), 'wb') as p:
+	pickle.dump(sag_hashes_set, p)
+'''
+with open(join(save_path, sag_id + '.pkl'), 'rb') as p:
+	sag_hashes = pickle.load(p)
+print('Unpickled SAG L-hashes')
 
-sag_files = [join(sag_path, f) for f in listdir(sag_path) if isfile(join(sag_path, f))]
+# MG Tetras
+mg_id = mg_file.split('/')[-1].split('.')[0]
+'''
+mg_contigs = get_seqs(mg_file)
+# Break up contigs into overlapping subseqs
+mg_headers, mg_subs = get_subseqs(mg_contigs, max_contig_len, overlap_len)
+mg_tetra_df = pd.DataFrame.from_dict(tetra_cnt(mg_subs))
+mg_tetra_df['contig_id'] = mg_headers
+mg_tetra_df.set_index('contig_id', inplace=True)
+print('MG %s tetranucleotide frequencies calculated' % mg_id)
+mg_tetra_df.to_csv(join(save_path, mg_id + '.tsv'), sep='\t')
+'''
+mg_tetra_df = pd.read_csv(join(save_path, mg_id + '.tsv'), sep='\t', index_col=0,
+						header=0)
+print('Opened MG tetranucleotide tsv file')
+'''
+# MG L-seg hash per fragged contig
+pass_list = []
+for mg_header, mg_frag in zip(mg_headers, mg_subs):
+	tmp, mg_Ls = get_subseqs([(mg_header, mg_frag)], 24, 23)
+	mg_hashes = calc_seg(mg_Ls)
+	mg_hashes.sort(reverse=True)
+	if any(i in sag_hashes for i in mg_hashes):
+		print('Fragment passed identity filter')
+		pass_list.append(mg_header)
+	else:
+		print('Failed')
+with open(join(save_path, mg_id + '.pkl'), 'wb') as p:
+	pickle.dump(pass_list, p)
+'''
+with open(join(save_path, mg_id + '.pkl'), 'rb') as p:
+	pass_list = pickle.load(p)
+print('Unpickled MG Pass Contigs')
+
+# Set indices for UMAP colorcoding
+sag_tetra_df['grouping'] = ['SAG' for x in sag_tetra_df.index]
+mg_new_index = []
+for index in mg_tetra_df.index:
+	if (index in pass_list) and ('NC_000913.3' in index):
+		mg_new_index.append('TruePos')
+	elif (index in pass_list) and ('NC_000913.3' not in index):
+			mg_new_index.append('FalsePos')
+	elif (index not in pass_list) and ('NC_000913.3' in index):
+		mg_new_index.append('FalseNeg')
+	elif (index not in pass_list) and ('NC_000913.3' not in index):
+		mg_new_index.append('TrueNeg')
+
+mg_tetra_df['grouping'] = mg_new_index
+
+concat_df = pd.concat([sag_tetra_df, mg_tetra_df])
+grouping = concat_df['grouping']
+sorter = ['TrueNeg', 'TruePos', 'SAG', 'FalseNeg', 'FalsePos']
+sorterIndex = dict(zip(sorter,range(len(sorter))))
+concat_df['Rank'] = concat_df['grouping'].map(sorterIndex)
+print(concat_df.groupby('grouping')[['aaaa']].count())
+concat_df.set_index('grouping', inplace=True)
+concat_df.sort_values(by=['Rank'], inplace=True)
+concat_df.drop(['Rank'], axis=1, inplace=True)
+
+features = concat_df.values
+targets = concat_df.index.values
+targets_ints = [x[0] for x in enumerate(targets, start=0)]
+
+
+data = plot_umap(concat_df, save_path, n_neighbors=30, min_dist=0.0,
+							n_components=2, random_state=42
+							)
+
+'''
+pca = PCA(0.99, whiten=True)
+data = pca.fit_transform(features)
+print(data.shape)
+'''
+
+n_components = np.arange(1, 136)
+models = [GMM(n, covariance_type='full', random_state=42).fit(data)
+          for n in n_components]
+plt.plot(n_components, [m.bic(data) for m in models], label='BIC')
+plt.plot(n_components, [m.aic(data) for m in models], label='AIC')
+plt.legend(loc='best')
+plt.xlabel('n_components');
+plot_file_name = sag_id + '.' + 'AIC_BIC.UMAP.png'
+plot_save_path = join(save_path, plot_file_name)
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
+
+gmm = GMM(n_components=15, covariance_type='full', random_state=42).fit(data)
+probs = gmm.predict_proba(data)
+probs_df = pd.DataFrame(data=probs.round(3), index=grouping)
+probs_df.reset_index(inplace=True)
+print(probs_df.groupby('grouping').sum())
+
+#print(probs_df.head())
+labels = gmm.predict(data)
+sizes = probs.max(1)  # square emphasizes differences
+
+ax = sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=targets)
+plt.gca().set_aspect('equal', 'datalim')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plot_file_name = sag_id + '.' + 'GMM_predict.UMAP.png'
+plot_save_path = join(save_path, plot_file_name)
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
+
+
+'''
+### Try UMAP ###
+plot_umap(concat_df, save_path)
+for n in (2, 5, 10, 20, 50, 100, 200):
+	plot_umap(concat_df, save_path, n_neighbors=n,
+				title='n_neighbors = {}'.format(n)
+				)
+for d in (0.0, 0.1, 0.25, 0.5, 0.8, 0.99):
+    plot_umap(concat_df, save_path, min_dist=d,
+    			title='min_dist = {}'.format(d)
+    			)
+
+for m in ('euclidean', 'l2', 'l1', 'manhattan', 'cityblock', 'braycurtis',
+			'canberra', 'chebyshev', 'correlation', 'cosine', 'dice',
+			'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching',
+			'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean',
+			'sokalmichener', 'sokalsneath', 'sqeuclidean'):
+	print(m)
+	plot_umap(concat_df, save_path, metric=m,
+				title='metric = {}'.format(m)
+				)
+'''
 
 # Process the SAGs
+'''
+sag_files = [join(sag_path, f) for f in listdir(sag_path) if isfile(join(sag_path, f))]
+
 sag_tetra_df_list = []
-for sag in sag_files[:25]:
+for sag in sag_files:
 	sag_id = sag.split('/')[-1].split('.')[0]
 	sag_contigs = get_seqs(sag)
 	# Break up contigs into overlapping subseqs
@@ -213,33 +410,106 @@ for sag in sag_files[:25]:
 	print('SAG %s tetranucleotide frequencies calculated' % sag_id)
 
 concat_df = pd.concat(sag_tetra_df_list)
+concat_df.to_csv(join(save_path, 'SAG_tetras.tsv'), sep='\t')
 
-### Try UMAP ###
-import numpy as np
-from sklearn.datasets import load_iris, load_digits
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+concat_df = pd.read_csv(join(save_path, 'SAG_tetras.tsv'), sep='\t', index_col=0,
+						header=0)
+'''
+'''
+mapper = umap.UMAP(n_neighbors=136).fit(features, targets_ints)
+test_embedding = mapper.transform(features)
+classes = list(set(targets))
 
-sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
-
-import umap
-
-plot_umap(concat_df)
-
-#for n in (2, 5, 10, 20, 50, 100, 200):
-#	plot_umap(concat_df, n_neighbors=n, title='n_neighbors = {}'.format(n))
-#for d in (0.0, 0.1, 0.25, 0.5, 0.8, 0.99):
-#    plot_umap(concat_df, min_dist=d, title='min_dist = {}'.format(d))
-
-### NEXT STEPS: Transforming New Data with UMAP ###
-### https://umap-learn.readthedocs.io/en/latest/transform.html ###
+fig, ax = plt.subplots(1, figsize=(14, 10))
+ax = sns.scatterplot(*mapper.embedding_.T, hue=targets)
+plt.gca().set_aspect('equal', 'datalim')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.title('Fashion MNIST Train Digits Embedded via UMAP Transform')
+plot_file_name = 'Training_data.png'
+plot_save_path = join(save_path, plot_file_name)
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
 
 
-
+fig, ax = plt.subplots(1, figsize=(14, 10))
+ax = sns.scatterplot(*mapper.embedding_.T, hue=targets)
+plt.gca().set_aspect('equal', 'datalim')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.title('Fashion MNIST Train Digits Embedded via UMAP')
+plot_file_name = 'Test_data.png'
+plot_save_path = join(save_path, plot_file_name)
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
+'''
 
 # Old code
+'''
+class KDEClassifier(BaseEstimator, ClassifierMixin):
+    """Bayesian generative classification based on KDE
+    
+    Parameters
+    ----------
+    bandwidth : float
+        the kernel bandwidth within each class
+    kernel : str
+        the kernel name, passed to KernelDensity
+    """
+    def __init__(self, bandwidth=1.0, kernel='gaussian'):
+        self.bandwidth = bandwidth
+        self.kernel = kernel
+        
+    def fit(self, X, y):
+        self.classes_ = np.sort(np.unique(y))
+        training_sets = [X[y == yi] for yi in self.classes_]
+        self.models_ = [KernelDensity(bandwidth=self.bandwidth,
+                                      kernel=self.kernel).fit(Xi)
+                        for Xi in training_sets]
+        self.logpriors_ = [np.log(Xi.shape[0] / X.shape[0])
+                           for Xi in training_sets]
+        return self
+        
+    def predict_proba(self, X):
+        logprobs = np.array([model.score_samples(X)
+                             for model in self.models_]).T
+        result = np.exp(logprobs + self.logpriors_)
+        return result / result.sum(1, keepdims=True)
+        
+    def predict(self, X):
+        return self.classes_[np.argmax(self.predict_proba(X), 1)]
+
+
+### KDE ###
+bandwidths = 10 ** np.linspace(0, 2, 100)
+grid = GridSearchCV(KDEClassifier(), {'bandwidth': bandwidths})
+grid.fit(features, targets)
+
+scores = [val.mean_validation_score for val in grid.grid_scores_]
+plt.semilogx(bandwidths, scores)
+plt.xlabel('bandwidth')
+plt.ylabel('accuracy')
+plt.title('KDE Model Performance')
+plot_save_path = join(save_path, 'KDE_model_perform.png')
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
+print(grid.best_params_)
+print('accuracy =', grid.cv_results_)
+'''
+### PCA AIC ###
+'''
+pca = PCA(0.99, whiten=True)
+data = pca.fit_transform(features)
+print(data.shape)
+
+n_components = np.arange(20, 150, 10)
+models = [GMM(n, covariance_type='full', random_state=0)
+          for n in n_components]
+aics = [model.fit(data).aic(data) for model in models]
+plt.plot(n_components, aics)
+plot_save_path = join(save_path, 'GMM_AIC.png')
+plt.savefig(plot_save_path, bbox_inches="tight")
+plt.clf()
+'''
+
 '''
 # Process the MetaG fasta
 mg_contigs = []
