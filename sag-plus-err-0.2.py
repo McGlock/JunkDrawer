b0f3d910e53083dc923590d6bfe653cb319f6c12
Ryan McLaughlin <rmclaughlin@shamwow.microbiology.ubc.ca>
2019-08-22 11:14:01 -0700

    created new sag-err ver 0.2 that uses bp count instead of simple contig count

--- sag-plus-err-0.2.py
+++ sag-plus-err-0.2.py
@@ -10,6 +10,7 @@ from functools import reduce
 import numpy as np
 from collections import Counter
 from os import listdir, makedirs, path
+from Bio import SeqIO
 
 
 def calc_err(df):
@@ -33,6 +34,42 @@ def calc_err(df):
 	stack_df = stats_df.stack().reset_index()
 	stack_df.columns = ['sag_id', 'algorithm', 'level', 'statistic', 'score']
 	return stack_df
+
+
+def get_seqs(fasta_file):
+	fa_recs = []
+	with open(fasta_file, 'r') as fasta_in:
+		for record in SeqIO.parse(fasta_in, 'fasta'):
+			f_id = record.id
+			f_description = record.description
+			f_seq = str(record.seq)
+			if f_seq != '':
+				fa_recs.append((f_id, f_seq))
+
+	return fa_recs
+
+
+def cnt_total_bp(fasta_file):
+	# counts total basepairs contained in file
+	# returns fasta_file name and total counts for entire fasta file
+
+	fasta_records = get_seqs(fasta_file)
+	bp_sum = 0
+	for f_rec in fasta_records:
+		bp_sum += len(f_rec[1])
+	return fasta_file, bp_sum
+
+
+def cnt_contig_bp(fasta_file):
+	# counts basepairs/read contained in file
+	# returns dictionary of {read_header:bp_count}
+
+	fasta_records = get_seqs(fasta_file)
+	fa_cnt_dict = {}
+	for f_rec in fasta_records:
+		fa_cnt_dict[f_rec[0]] = len(f_rec[1])
+
+	return fa_cnt_dict
 
 
 # Map genome id and contig id to taxid for error analysis
@@ -68,6 +105,68 @@ files_path = sys.argv[1]
 err_path = files_path + '/error_analysis'
 if not path.exists(err_path):
 	makedirs(err_path)
+
+# count all bp's for Source genomes, Source MetaG, MockSAGs
+src_metag_file = '/home/rmclaughlin/Ryan/CAMI_I_HIGH/CAMI_high_GoldStandardAssembly.fasta'
+src_genome_path = '/home/rmclaughlin/Ryan/CAMI_I_HIGH/source_genomes/'
+mocksag_path = files_path + 'mockSAGs/'
+# list all source genomes
+src_genome_list = [joinpath(src_genome_path, f) for f in listdir(src_genome_path)
+			if ((f.split('.')[-1] == 'fasta' or f.split('.')[-1] == 'fna') and
+				'Sample' not in f)
+			]
+# list all mockSAGs
+mocksag_list = [joinpath(mocksag_path, f) for f in listdir(mocksag_path)
+			if (f.split('.')[-1] == 'fasta')
+				]
+
+src_mock_list = src_genome_list + mocksag_list
+
+# count total bp's for each src and mock fasta
+fa_bp_cnt_list = []
+for fa_file in src_mock_list:
+	if '.mockSAG.fasta' in fa_file:
+		f_id = fa_file.split('/')[-1].split('.mockSAG.fasta')[0]
+		f_type = 'mockSAG'
+	else:
+		f_id = fa_file.split('/')[-1].split('.fasta')[0]
+		f_type = 'src_genome'
+	fa_file, fa_bp_cnt = cnt_total_bp(fa_file)
+	fa_bp_cnt_list.append([f_id, f_type, fa_bp_cnt])
+fa_bp_cnt_df = pd.DataFrame(fa_bp_cnt_list, columns=['sag_id', 'data_type', 'tot_bp_cnt'])
+unstack_cnt_df = fa_bp_cnt_df.set_index(['sag_id', 'data_type']).unstack(level=-1).reset_index()
+unstack_cnt_df.columns = ['sag_id', 'mockSAG_tot', 'src_genome_tot']
+# calc basic stats for src and mock
+src_mock_err_list = []
+for ind, row in unstack_cnt_df.iterrows():
+	sag_id = row['sag_id']
+	mockSAG_tot = row['mockSAG_tot']
+	src_genome_tot = row['src_genome_tot']
+	data_type_list = ['mockSAG', 'src_genome']
+	for dt in data_type_list:
+		algorithm = dt
+		for level in ['genus', 'species', 'strain', 'perfect']:
+			s_m_err_list = [sag_id, algorithm, level, 0, 0, 0, 0]
+			if dt == 'mockSAG':
+				s_m_err_list[3] += mockSAG_tot # 'TruePos'
+				s_m_err_list[4] += 0 # 'FalsePos'
+				s_m_err_list[5] += src_genome_tot - mockSAG_tot # 'FalseNeg'
+				s_m_err_list[6] += 0 # 'TrueNeg'
+				src_mock_err_list.append(s_m_err_list)
+			#else:
+			#	s_m_err_list[3] += src_genome_tot # 'TruePos'
+			#	s_m_err_list[4] += 0 # 'FalsePos'
+			#	s_m_err_list[5] += 0 # 'FalseNeg'
+			#	s_m_err_list[6] += 0 # 'TrueNeg'
+			#src_mock_err_list.append(s_m_err_list)
+
+src_mock_err_df = pd.DataFrame(src_mock_err_list, columns=['sag_id', 'algorithm', 'level',
+													'TruePos', 'FalsePos',
+													'FalseNeg', 'TrueNeg'
+													])
+
+# count all bp's for each read in metaG
+src_metag_cnt_dict = cnt_contig_bp(src_metag_file)
 
 # MinHash
 mh_path = joinpath(files_path, 'minhash_recruits/')
@@ -141,8 +240,7 @@ final_tax_df = final_group_df.merge(tax_mg_df, left_on='contig_id', right_on='@@
 								how='left'
 								)
 sag_cnt_dict = final_tax_df.groupby('sag_id')['sag_id'].count().to_dict()
-#final_tax_df['contig_count'] = [sag_cnt_dict[x] for x in final_tax_df['sag_id']]
-#final_tax_df.sort_values('contig_count', ascending=True, inplace=True)
+
 error_list = []
 algo_list = ['MinHash', 'RPKM', 'tetra_GMM', 'combined']
 level_list = ['genus', 'species', 'strain', 'CAMI_genomeID']
@@ -173,29 +271,31 @@ for i, sag_id in enumerate(list(final_df['sag_id'].unique())):
 				col_key = sag_key
 			err_list = [sag_id, algo, col, 0, 0, 0, 0]
 			for contig_id in tax_mg_df['@@SEQUENCEID']:
+				contig_count = src_metag_cnt_dict[contig_id]
 				if contig_id in list(algo_sub_df['contig_id']):
 					if contig_id in mg_include_contigs:
-						err_list[3] += 1 # 'TruePos'
+						err_list[3] += contig_count # 'TruePos'
 					else:
-						err_list[4] += 1 # 'FalsePos'
+						err_list[4] += contig_count # 'FalsePos'
 				else:
 					if contig_id in sag_include_contigs:
-						err_list[5] += 1 # 'FalseNeg'
+						err_list[5] += contig_count # 'FalseNeg'
 					else:
-						err_list[6] += 1 # 'TrueNeg'
+						err_list[6] += contig_count # 'TrueNeg'
 			error_list.append(err_list)
 
-final_err_df = pd.DataFrame(error_list, columns=['sag_id', 'algorithm', 'level',
+mg_err_df = pd.DataFrame(error_list, columns=['sag_id', 'algorithm', 'level',
 													'TruePos', 'FalsePos',
 													'FalseNeg', 'TrueNeg'
 													])
+final_err_df = pd.concat([src_mock_err_df, mg_err_df])
 
-final_err_df.to_csv(err_path + '/All_error.tsv', index=False, sep='\t')
+final_err_df.to_csv(err_path + '/All_error_count.tsv', index=False, sep='\t')
 
 calc_stats_df = calc_err(final_err_df)
 stat_list = ['precision', 'sensitivity', 'F1_score']
 calc_stats_df = calc_stats_df.loc[calc_stats_df['statistic'].isin(stat_list)]
-calc_stats_df.to_csv(err_path + '/All_stats.tsv', index=False, sep='\t')
+calc_stats_df.to_csv(err_path + '/All_stats_count.tsv', index=False, sep='\t')
 for level in set(calc_stats_df['level']):
 	level_df = calc_stats_df.loc[calc_stats_df['level'] == level]
 	sns.set_context("paper")
@@ -210,7 +310,7 @@ for level in set(calc_stats_df['level']):
 	plt.ylim(0, 1)
 	plt.title('SAG-plus CAMI-1-High error analysis')
 	ax._legend.set_title('Filter Type')
-	plt.savefig(err_path + '/' + level + '_error_boxplox.svg',
+	plt.savefig(err_path + '/' + level + '_error_boxplox_count.svg',
 				bbox_inches='tight'
 				)
 	plt.clf()
@@ -218,23 +318,29 @@ for level in set(calc_stats_df['level']):
 # build multi-level precision boxplot
 level_list = ['genus', 'species', 'strain', 'perfect']
 stat_list = ['precision', 'sensitivity', 'F1_score']
-comb_stat_df = calc_stats_df.loc[((calc_stats_df['algorithm'] == 'combined') & 
+comb_stat_df = calc_stats_df.loc[((calc_stats_df['algorithm'].isin(['combined'])) & 
 									(calc_stats_df['level'].isin(level_list)) &
 									(calc_stats_df['statistic'].isin(stat_list))
 									)]
+mock_stat_df = calc_stats_df.loc[((calc_stats_df['algorithm'].isin(['mockSAG'])) & 
+									(calc_stats_df['level'].isin(['genus'])) &
+									(calc_stats_df['statistic'].isin(stat_list))
+									)]
+mock_stat_df['level'] = 'mockSAG'
+concat_stat_df = pd.concat([mock_stat_df, comb_stat_df])
 sns.set_context("paper")
-ax = sns.catplot( x="level", y="score", hue='statistic', kind='box',
-					data=comb_stat_df, aspect=2
+ax = sns.catplot(x="level", y="score", hue='statistic', kind='box',
+					data=concat_stat_df, aspect=2
 					)
 
-plt.plot([-1, 4], [0.25, 0.25], linestyle='--', alpha=0.3, color='k')
-plt.plot([-1, 4], [0.50, 0.50], linestyle='--', alpha=0.3, color='k')
-plt.plot([-1, 4], [0.75, 0.75], linestyle='--', alpha=0.3, color='k')
+plt.plot([-1, 5], [0.25, 0.25], linestyle='--', alpha=0.3, color='k')
+plt.plot([-1, 5], [0.50, 0.50], linestyle='--', alpha=0.3, color='k')
+plt.plot([-1, 5], [0.75, 0.75], linestyle='--', alpha=0.3, color='k')
 
 plt.ylim(0, 1)
 plt.title('SAG-plus CAMI-1-High')
 
-plt.savefig(err_path + '/multi-level_precision_boxplox.svg',
+plt.savefig(err_path + '/multi-level_precision_boxplox_count.svg',
 				bbox_inches='tight'
 				)
 plt.clf()
